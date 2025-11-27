350473bd563859e2a7cbb56bb07e1de0bf2ba78b
Gnanesh <gnaneshmahi4@gmail.com>
2025-11-27 23:37:41 +0530

    ReactJs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-let App=()=>{
+//  function App(){
+//     return <div>
+//         <h4>Gnanesh</h4>
+//         <h1>Hello-Good Morning</h1>
+//     </div>
+// }
+
+
+
+import Header from "./components/Header";
+import Footer from './components/Footer'
+function App(){
+
     return <div>
-        <h4>Gnanesh</h4>
-        <h1>Hello-Good Morning</h1>
-    </div>
+            <h1>App Component</h1>
+            <hr />
+            <Header/>
+            <Footer/>
+           </div>
 }
+
 export default App;
